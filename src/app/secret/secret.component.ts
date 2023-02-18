0f73739aac3a8dce517371a119e1ecd26282d7ee
zhulishen <zhulishen0331@gmail.com>
2023-02-18 14:09:54 +0900

    complete

--- src/app/secret/secret.component.ts
+++ src/app/secret/secret.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { WeatherClient } from '../clients/weather.client';
+import { AuthenticationService } from '../services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-secret',
   templateUrl: './secret.component.html',
   styleUrls: ['./secret.component.css']
 })
-export class SecretComponent {
 
+export class SecretComponent implements OnInit {
+  public weather: Observable<any> = this.weatherClient.getWeatherData();
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private weatherClient: WeatherClient
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  logout(): void {
+    this.authenticationService.logout();
+  }
 }
